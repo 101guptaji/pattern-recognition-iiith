79d792c6757d213fd88956008b452eb6e3403805
101guptaji <101guptaji@gmail.com>
2019-06-06 21:51:26 +0530

    demo-1 of data in class 1

--- SRIP/Codes/layout.js
+++ SRIP/Codes/layout.js
@@ -1,19 +1,21 @@
 window.onload = function () {
 
-var chart = new CanvasJS.Chart("chartContainer", {
+global var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	
 	axisX: {
 		title:"X-axis",
 		lineThickness: 2,
 		minimum: -1.1,
-		maximum: 1.1
+		maximum: 1.1,
+		split: 0.25
 	},
 	axisY:{
 		title: "Y-axis",
 		lineThickness: 2,
 		minimum: -1.1,
-		maximum: 1.1
+		maximum: 1.1,
+		split: 0.1
 	},
 	data: [{
 		type: "scatter",
@@ -37,4 +39,18 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	}]
 });
 chart.render();
+
+
+function class1active() {
+	
+	var length = chart.options.data.dataPoints.length;
+	chart.options.data.type: "scatter",
+	chart.options.data.toolTipContent: "<span style=\"color:blue \"><b>{name}</b></span><br/><b> X-axis:</b> {x}<br/><b> Y-axis:</b></span> {y} ",
+	chart.options.data.name: "Class-1",
+	chart.options.data.showInLegend: true,
+	chart.options.data.dataPoints.push({x: 0.25, y: -0.25});
+	chart.render();
+
+	};
+
 }
